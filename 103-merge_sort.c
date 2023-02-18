525e4f11fa6ba504aac08c357ee27d92cfb9a138
girmesh03 <girmachewzewdie03@gmail.com>
2023-02-18 19:59:11 +0300

    103-merge_sort.c modified

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -3,7 +3,6 @@
 void merge(int *array, int *left_array, int *right_array,
 		   int left_size, int right_size, int size);
 void merge_sort(int *array, size_t size);
-
 /**
  * merge - merge two arrays
  * @array: array to sort
@@ -13,7 +12,6 @@ void merge_sort(int *array, size_t size);
  * @right_size: size of right array
  * @size: size of the array
  */
-
 void merge(int *array, int *left_array, int *right_array,
 		   int left_size, int right_size, int size)
 {
@@ -21,7 +19,6 @@ void merge(int *array, int *left_array, int *right_array,
 	int i = 0, j = 0, k = 0;
 
 	printf("Merging...\n");
-
 	while (i < left_size && j < right_size)
 	{
 		if (left_array[i] <= right_array[j])
@@ -33,39 +30,38 @@ void merge(int *array, int *left_array, int *right_array,
 			temp[k++] = right_array[j++];
 		}
 	}
-
-	while (i < left_size)
+	if (i > left_size)
 	{
-		temp[k++] = left_array[i++];
+		while (j < right_size)
+		{
+			temp[k++] = right_array[j++];
+		}
 	}
-
-	while (j < right_size)
+	else
 	{
-		temp[k++] = right_array[j++];
+		while (i <= left_size)
+		{
+			temp[k++] = left_array[i++];
+		}
 	}
-
 	printf("[left]: ");
 	print_array(left_array, left_size);
 	printf("[right]: ");
 	print_array(right_array, right_size);
 	printf("[Done]: ");
 	print_array(temp, size);
-
 	for (i = 0; i < size; i++)
 	{
 		array[i] = temp[i];
 	}
-
 	free(temp);
 }
-
 /**
  * merge_sort - sorts an array of integers in ascending order
  * using the Merge Sort algorithm
  * @array: array to sort
  * @size: size of the array
  */
-
 void merge_sort(int *array, size_t size)
 {
 	int *left_array, *right_array;
